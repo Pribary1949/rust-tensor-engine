997d42f80b38590306385282c15a4421f1c4550a
Marcus J. Sterling <inaradarem@gmx.net>
2023-10-15 14:30:00 -0400

    Feature implementation: Part 1 of core rust-tensor-engine modules

--- src/tensor.rs
+++ src/tensor.rs
@@ -1,17 +1,23 @@
+use std::ops::{Add, Mul};
+
+#[derive(Debug, Clone)]
 pub struct Tensor {
     pub data: Vec<f32>,
     pub shape: Vec<usize>,
+    pub strides: Vec<usize>,
 }
 
 impl Tensor {
-    pub fn new(shape: Vec<usize>) -> Self {
-        let size = shape.iter().product();
-        Self { data: vec![0.0; size], shape }
+    pub fn new(data: Vec<f32>, shape: Vec<usize>) -> Self {
+        let strides = Self::compute_strides(&shape);
+        assert_eq!(data.len(), shape.iter().product::<usize>(), "Data length mismatch");
+        Tensor { data, shape, strides }
     }
 
-    pub fn matmul(&self, other: &Tensor) -> Result<Tensor, String> {
-        // Matrix multiplication logic
-        if self.shape[1] != other.shape[0] { return Err("Invalid dimensions".into()); }
-        Ok(Tensor::new(vec![self.shape[0], other.shape[1]]))
+    pub fn zeros(shape: Vec<usize>) -> Self {
+        let size = shape.iter().product();
+        Self::new(vec![0.0; size], shape)
     }
-}
+
+    fn compute_strides(shape: &[usize]) -> Vec<usize> {
+        let mut strides = vec![1; shape.len()];
